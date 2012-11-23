b2a83f70fa859c286b983a5d872a59be621560bb
Aaron1178 <Aaron1178@gmail.com>
2012-11-23 12:38:26 +1100

    C++ version of Nif Explorer

---
+++ NifExplorer/Nif Explorer/NifExplorer.h
@@ -0,0 +1,13 @@
+#ifndef NIF_EXPLORER
+#define NIF_EXPLORER
+
+#include <iostream>
+#include <string.h>
+#include <Windows.h>
+#include "niflib.h"
+#include "obj/NiObject.h"
+#include "obj/NiNode.h"
+
+using namespace Niflib;	
+
+#endif
