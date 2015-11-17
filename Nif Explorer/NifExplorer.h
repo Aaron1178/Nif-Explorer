6c9a1c5d0c2f72331906a7d36d07f7b76382ab7e
Aaron1178 <Aaron1178@gmail.com>
2015-11-17 18:31:13 +1100

    t

---
+++ Nif Explorer/NifExplorer.h
@@ -0,0 +1,19 @@
+/*
+* By Aaron1178
+*
+*/
+
+#ifndef NIF_EXPLORER
+#define NIF_EXPLORER
+
+#include <iostream>
+#include <string.h>
+#include <Windows.h>
+#include "niflib.h"
+#include "gen/Header.h"
+#include "obj/NiObject.h"
+#include "obj/NiNode.h"
+
+using namespace Niflib;	
+
+#endif
